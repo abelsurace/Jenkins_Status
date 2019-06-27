833087ac86ed45330acea83ca958d97a6adc8976
abelsurace <abelsurace@yahoo.com>
2019-06-27 13:21:28 -0600

    adding MQTT observer to publish build status

---
+++ config/initcommmands.sh
@@ -0,0 +1,3 @@
+amixer set PCM -- 100%  #set voulem to 100%
+export PATH=$PATH:/home/pi/.local/bin  #  make google-speach location in the PATH
+sudo openvpn /etc/openvpn/client.conf  #execute open vpn
